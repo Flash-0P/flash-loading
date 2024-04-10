bdd8c884c2da4ce9f4cf27c228f7c3812c0b4dc4
Flash OP <128111282+Flash-0p@users.noreply.github.com>
2024-04-09 18:26:52 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Killer-loading by Flash-OP'
+version '1.2.0'
+
+
+files {
+  'assets/**',
+  'html/*'
+}
+
+loadscreen {
+  'html/index.html'
+}
+
+loadscreen_cursor 'no'
+loadscreen_manual_shutdown 'yes'
+
+lua54 'yes'
---
+++ html/app.js
@@ -0,0 +1,83 @@
+// FLASH-OP
+
+const { ref } = Vue
+
+// Customize language for dialog menus and carousels here
+
+const load = Vue.createApp({
+  setup () {
+    return {
+      DownloadTitle: 'Downloading Server Data',
+      DownloadDesc: "Hold tight while we begin downloading all the resources/assets required to play on QBCore Server. \n\nAfter download has been finished successfully, you'll be placed into the server and this screen will disappear. Please don't leave or turn off your PC. ",
+
+      SettingsTitle: 'Settings',
+      AudioTrackDesc1: 'When disabled the current audio-track playing will be stopped.',
+      AutoPlayDesc2: 'When disabled carousel images will stop cycling and remain on the last shown.',
+      PlayVideoDesc3: 'When disabled video will stop playing and remain paused.',
+
+      firstap: ref(true),
+      secondap: ref(true),
+      thirdap: ref(true),
+      firstslide: ref(1),
+      secondslide: ref('1'),
+      thirdslide: ref('5'),
+      audioplay: ref(true),
+      playvideo: ref(true),
+      download: ref(true),
+      settings: ref(false),
+    }
+  }
+})
+
+load.use(Quasar, { config: {} })
+load.mount('#loading-main')
+
+var audio = document.getElementById("audio");
+audio.volume = 0.05;
+
+function audiotoggle() {
+    var audio = document.getElementById("audio");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+function videotoggle() {
+    var video = document.getElementById("video");
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+let count = 0;
+let thisCount = 0;
+
+const handlers = {
+    startInitFunctionOrder(data) {
+        count = data.count;
+    },
+
+    initFunctionInvoking(data) {
+        document.querySelector(".thingy").style.left = "0%";
+        document.querySelector(".thingy").style.width = (data.idx / count) * 100 + "%";
+    },
+
+    startDataFileEntries(data) {
+        count = data.count;
+    },
+
+    performMapLoadFunction(data) {
+        ++thisCount;
+
+        document.querySelector(".thingy").style.left = "0%";
+        document.querySelector(".thingy").style.width = (thisCount / count) * 100 + "%";
+    },
+};
+
+window.addEventListener("message", function (e) {
+    (handlers[e.data.eventName] || function () {})(e.data);
+});
